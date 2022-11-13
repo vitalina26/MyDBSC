d47e9345d52dcda70e36afa8d9c975431f70da97
vitalina26 <73598254+vitalina26@users.noreply.github.com>
2022-11-13 18:03:35 +0200

    Create Row.cs

---
+++ MyClassesForDBManager/Row.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesForBD
+{
+    class Row
+    {
+        public Row() { }
+        public List<string> Values { get; set; } = new();
+    }
+}
